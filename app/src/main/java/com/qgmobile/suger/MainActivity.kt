75fad41ddec6dac24516a2566a77485879fd1efe
jim ji <1035363309@qq.com>
2017-09-23 16:36:39 +0800

    add adapter (no use)

--- app/src/main/java/com/qgmobile/suger/MainActivity.kt
+++ app/src/main/java/com/qgmobile/suger/MainActivity.kt
@@ -2,18 +2,35 @@ package com.qgmobile.suger
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import com.mobile.utils.*
 
-import com.mobile.utils.fullScreen
-import com.mobile.utils.setStatusBarTextBlack
-import com.mobile.utils.setStatusBarTextWhite
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item.view.*
 
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var adapter: EasyAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fullScreen()
-        setStatusBarTextBlack()
+        setStatusBarTextWhite()
+
+        adapter = EasyAdapter()
+        adapter.addConfig(EasyAdapter.ItemConfig(String::class, 1) {
+            layId = R.layout.item
+            onBindData { data, holder -> holder.itemView.textView.text = data.toString() }
+            onClick { data, pos -> showToast("$pos  $data") }
+        })
+        adapter.addConfig(EasyAdapter.ItemConfig(Int::class, 2) {
+            layId = R.layout.item2
+            onBindData { data, holder -> holder.itemView.textView.text = data.toString() }
+            onClick { data, pos -> showToast("$pos  $data") }
+        })
+        adapter.addData(listOf("wdsda", 1, 3, "6345"))
+        list.layoutManager = LinearLayoutManager(this)
+        list.adapter = adapter
+
     }
 }
---
+++ utils/src/main/java/com/mobile/utils/EasyAdapter.kt
@@ -0,0 +1,140 @@
+package com.mobile.utils
+
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlin.reflect.KClass
+
+/**
+ * Created by jimji on 2017/9/23.
+ */
+class EasyAdapter : RecyclerView.Adapter<EasyAdapter.ViewHolder>() {
+    companion object {
+        val HEADER = Int.MAX_VALUE - 1
+        val FOOTER = Int.MAX_VALUE - 2
+    }
+
+    private val mItemConfigs = mutableMapOf<Int, ItemConfig>()
+    private val mDatas = mutableListOf<Any>()
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val data = mDatas[position]
+        mItemConfigs.map { entry ->
+            if (entry.value.dataClass == data::class) {
+                entry.value._bindData(data, holder)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = mDatas.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val config = mItemConfigs[viewType]
+        if (config != null) {
+            val itemView = LayoutInflater.from(parent.context).inflate(config.layId, parent, false)
+            val holder = ViewHolder(itemView)
+            itemView.setOnClickListener {
+                config._onClick(mDatas[holder.adapterPosition], holder.adapterPosition)
+            }
+            return holder
+        }
+        throw RuntimeException("viewType not found")
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        mItemConfigs.map { entry ->
+            if (entry.value.dataClass == mDatas[position]::class) return entry.value.type
+        }
+        throw RuntimeException("viewType not found")
+    }
+
+    override fun onViewRecycled(holder: ViewHolder) {
+        val data = mDatas[holder.adapterPosition]
+        mItemConfigs.map {
+            if (it.value.dataClass == data::class) {
+                it.value._onRecycle(data, holder)
+            }
+        }
+        super.onViewRecycled(holder)
+    }
+
+    fun addConfig(config: ItemConfig) {
+        mItemConfigs.put(config.type, config)
+    }
+
+    fun addData(data: Any) {
+        val lastIndex = if (mDatas.size == 0) 0 else mDatas.size - 1
+        mDatas.add(data)
+        notifyDataSetChanged()
+    }
+
+    fun addData(datas: List<Any>) {
+        val lastIndex = if (mDatas.size == 0) 0 else mDatas.size - 1
+        mDatas.addAll(datas)
+        notifyDataSetChanged()
+    }
+
+    fun addData(index: Int, data: Any) {
+        mDatas.add(index, data)
+        notifyItemInserted(index)
+    }
+
+    fun addDataRange(indexStart: Int, datas: List<Any>) {
+        mDatas.addAll(indexStart, datas)
+        notifyItemRangeInserted(indexStart, datas.size)
+    }
+
+    fun update(newDatas: List<Any>) {
+        mDatas.clear()
+        mDatas.addAll(newDatas)
+        notifyDataSetChanged()
+    }
+
+    fun removeData(index: Int) {
+        if (mDatas.size == 0) return
+        mDatas.removeAt(index)
+        notifyItemRemoved(index)
+    }
+
+    fun removeData(range: IntRange) {
+        if (mDatas.size - 1 < range.last || range.last < range.first) return
+        range.forEach { mDatas.removeAt(range.first) }
+        notifyItemRangeRemoved(range.first, range.last - range.first + 1)
+    }
+
+    fun clearData() {
+        mDatas.clear()
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(var itemView: View) : RecyclerView.ViewHolder(itemView) {
+        //什么都可以，你想留着的东西
+        lateinit var tmp: Any
+    }
+
+    open class ItemConfig(val dataClass: KClass<*>, val type: Int, setup: ItemConfig.() -> Unit) {
+        var layId: Int = 0
+        var _bindData: (data: Any, holder: RecyclerView.ViewHolder) -> Unit = { _, _ -> }
+        var _onClick: (data: Any, pos: Int) -> Unit = { _, _ -> }
+        var _onRecycle: (Any, ViewHolder) -> Unit = { _, _ -> }
+        fun onClick(c: (data: Any, pos: Int) -> Unit) {
+            _onClick = c
+        }
+
+        fun onBindData(b: (data: Any, holder: RecyclerView.ViewHolder) -> Unit) {
+            _bindData = b
+        }
+
+        fun onRecycle(r: (Any, ViewHolder) -> Unit) {
+            _onRecycle = r
+        }
+
+        init {
+            setup()
+        }
+    }
+
+
+}
