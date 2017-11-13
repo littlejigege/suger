23a8eaff1c92ef5ea7e267ea59ff48d8bee1f39a
weeChanc <214652773@qq.com>
2017-11-13 19:47:04 +0800

    修改了ImageUtils

--- utils/src/main/java/com/mobile/utils/ImageUtils.kt
+++ utils/src/main/java/com/mobile/utils/ImageUtils.kt
@@ -54,51 +54,65 @@ fun Bitmap.scale(newWidth: Int, newHeight: Int): Bitmap {
     return ret
 }
 
-fun Bitmap.compressByQuality(maxByteSize: Long): Bitmap? {
-    if (maxByteSize <= 0) return null
-    val baos = ByteArrayOutputStream()
-    var quality = 100
-    compress(Bitmap.CompressFormat.JPEG, quality, baos)
-    while (baos.toByteArray().size > maxByteSize && quality > 0) {
-        baos.reset()
-        compress(Bitmap.CompressFormat.JPEG, { quality -= 5;quality }(), baos)
+
+fun Bitmap.size() = rowBytes * byteCount
+
+
+class ImageUtils {
+    companion object {
+
+        fun gaussBlud(bitmap: Bitmap,blurRadius : Float = 20f ): Bitmap {
+
+            val BITMAP_SCALE = 0.4f
+            val width = Math.round(bitmap.getWidth() * BITMAP_SCALE)
+            val height = Math.round(bitmap.getHeight() * BITMAP_SCALE)
+
+            // 将缩小后的图片做为预渲染的图片
+            val inputBitmap = Bitmap.createScaledBitmap(bitmap, width, height, false)
+            // 创建一张渲染后的输出图片
+            val outputBitmap = Bitmap.createBitmap(inputBitmap)
+
+            // 创建RenderScript内核对象
+            val rs = RenderScript.create(Utils.app)
+            // 创建一个模糊效果的RenderScript的工具对象
+            val blurScript = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs))
+
+            // 由于RenderScript并没有使用VM来分配内存,所以需要使用Allocation类来创建和分配内存空间
+            // 创建Allocation对象的时候其实内存是空的,需要使用copyTo()将数据填充进去
+            val tmpIn = Allocation.createFromBitmap(rs, inputBitmap);
+            val tmpOut = Allocation.createFromBitmap(rs, outputBitmap)
+
+            // 设置渲染的模糊程度, 25f是最大模糊度
+            blurScript.setRadius(blurRadius)
+            // 设置blurScript对象的输入内存
+            blurScript.setInput(tmpIn)
+            // 将输出数据保存到输出内存中
+            blurScript.forEach(tmpOut)
+
+            // 将数据填充到Allocation中
+            tmpOut.copyTo(outputBitmap)
+
+            return outputBitmap
+        }
+
+        fun compressByQuality(bitmap: Bitmap,maxByteSize: Long): Bitmap? {
+            if (maxByteSize <= 0) return null
+            val baos = ByteArrayOutputStream()
+            var quality = 100
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, baos)
+            while (bitmap.size() > maxByteSize && quality > 0) {
+                baos.reset()
+                bitmap.compress(Bitmap.CompressFormat.JPEG, { quality -= 5;quality }(), baos)
+            }
+            if (quality < 0) return null
+            val bytes = baos.toByteArray()
+            return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+        }
+
+
+
     }
-    if (quality < 0) return null
-    val bytes = baos.toByteArray()
-    return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
 }
 
-fun Bitmap.gaussBlud(blurRadius : Float = 20f ): Bitmap {
 
-    val BITMAP_SCALE = 0.4f
-    val width = Math.round(this.getWidth() * BITMAP_SCALE)
-    val height = Math.round(this.getHeight() * BITMAP_SCALE)
-
-    // 将缩小后的图片做为预渲染的图片
-    val inputBitmap = Bitmap.createScaledBitmap(this, width, height, false)
-    // 创建一张渲染后的输出图片
-    val outputBitmap = Bitmap.createBitmap(inputBitmap)
-
-    // 创建RenderScript内核对象
-    val rs = RenderScript.create(Utils.app)
-    // 创建一个模糊效果的RenderScript的工具对象
-    val blurScript = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs))
-
-    // 由于RenderScript并没有使用VM来分配内存,所以需要使用Allocation类来创建和分配内存空间
-    // 创建Allocation对象的时候其实内存是空的,需要使用copyTo()将数据填充进去
-    val tmpIn = Allocation.createFromBitmap(rs, inputBitmap);
-    val tmpOut = Allocation.createFromBitmap(rs, outputBitmap)
-
-    // 设置渲染的模糊程度, 25f是最大模糊度
-    blurScript.setRadius(blurRadius)
-    // 设置blurScript对象的输入内存
-    blurScript.setInput(tmpIn)
-    // 将输出数据保存到输出内存中
-    blurScript.forEach(tmpOut)
-
-    // 将数据填充到Allocation中
-    tmpOut.copyTo(outputBitmap)
-
-    return outputBitmap
-}
 
